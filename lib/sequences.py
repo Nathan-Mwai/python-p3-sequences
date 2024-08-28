5cd8ae74239ae3f96a4844bc85ac883c4435741e
Nathan Mwai <nathan.mwai@student.moringaschool.com>
2024-08-28 18:26:59 +0300

    completes lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
+def the_sequence(length):
+    sequence = [0, 1]
+    while len(sequence) < length:
+        sequence.append(sequence[-1] + sequence[-2])
+    return sequence
+
 
 def print_fibonacci(length):
+    solution = the_sequence(length)
+    if length == 0:
+        print([])
+    elif length == 1:
+        print([solution[0]])
+    else:
+        print(solution)
     pass
